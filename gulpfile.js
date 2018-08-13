ad8d48534ea7762eb42a942d9f54e17773346c53
Seno <iam@seno.me>
2018-08-14 01:12:51 +1200

    Added views, auto reload browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-
+	bs = require('browser-sync').create(),
 	$ = require('gulp-load-plugins')({
 		pattern: '*',
 		lazy: true
@@ -13,15 +13,30 @@ var gulp = require('gulp'),
 gulp.task('styles', function(){
 	return gulp.src(_.src + '/sass/**/*.sass')
 		.pipe($.sass().on('error', $.sass.logError))
-		.pipe(gulp.dest(_.build + '/css'));
-		// .pipe($.browserSync.create().stream();
+		.pipe(gulp.dest(_.build + '/css'))
+		.pipe(bs.stream());
 });
 
 gulp.task('markup', function(){
 	return gulp.src(_.src + '/views/**/!(_)*.pug')
-		.pipe($.pug({
-			pretty: true
-		}))
-		.pipe(gulp.dest(_.build));
+	.pipe($.pug({
+		pretty: true
+	}))
+	.pipe(gulp.dest(_.build));
 });
 
+// create a task that ensures the `markup` task is complete before
+// reloading browsers
+gulp.task('markup-watch', ['markup'], function(done){
+	bs.reload();
+	done();
+});
+
+gulp.task('server', ['markup', 'styles'], function(){
+	bs.init({
+		server: _.build
+	});
+
+	gulp.watch(_.src + '/sass/**/*.sass', ['styles']);
+	gulp.watch(_.src + '/views/**/!(_)*.pug', ['markup-watch']);
+});
