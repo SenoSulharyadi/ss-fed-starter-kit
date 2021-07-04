e609e192b13d1aee4116b9589bfc37d663dc25f8
Seno <borlandseno@yahoo.com>
2021-07-04 22:58:34 +1200

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,42 @@ const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 
 module.exports = {
     entry: {
+        // Path to file
         main: ['./themes/simple/sass/main.scss']
     },
     output: {
-        path: path.resolve(__dirname, 'themes/simple/css'),
+        path: path.join(__dirname, 'dist')
     },
     module: {
         rules: [
+            // Compile SASS to CSS
             {
                 test: /\.s[ac]ss$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    'css-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false, // Don't resolve url, https://stackoverflow.com/questions/42575712/webpack-sass-cannot-resolve-images
+                        }
+                    },
                     'postcss-loader',
-                    'sass-loader'
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
                 ],
             }
         ],
     },
     plugins: [
+        // Removed js file
         new FixStyleOnlyEntriesPlugin(),
+        // Extract css plugin option
         new MiniCssExtractPlugin({
-            filename: '[name].css'
+            filename: 'css/[name].css'
         })
     ]
 }
