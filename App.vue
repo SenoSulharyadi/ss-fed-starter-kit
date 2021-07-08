11494734caf1f39d09d6bc134c3fa74f0ae26087
Seno <borlandseno@yahoo.com>
2021-07-08 12:21:52 +1200

    Webpack Vue kit

---
+++ App.vue
@@ -0,0 +1,5 @@
+<template>
+    <section>
+        <h1>From Vue template</h1>
+    </section>
+</template>
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,23 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
     entry: {
-        // Path to file
-        main: ['./themes/simple/sass/main.scss']
-    },
-    output: {
-        path: path.join(__dirname, 'dist')
+        index: './index.js'
     },
     module: {
         rules: [
-            // Compile SASS to CSS
             {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false, // Don't resolve url, https://stackoverflow.com/questions/42575712/webpack-sass-cannot-resolve-images
-                        }
-                    },
-                    'postcss-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ],
+                test: /\.vue$/,
+                use: ['vue-loader']
             }
-        ],
+        ]
     },
     plugins: [
-        // Removed js file
-        new FixStyleOnlyEntriesPlugin(),
-        // Extract css plugin option
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css'
-        })
-    ]
+        new VueLoaderPlugin()
+    ],
+    devServer: {
+        contentBase: './',
+        port: 9000,
+        hot: true
+    },
 }
